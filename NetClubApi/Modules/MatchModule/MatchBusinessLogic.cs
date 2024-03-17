6e449ad9b4e217145c8c0082ce04499547a6e0e2
Abishek Aranganathan <abisheka@avecsage.com>
2024-03-17 16:04:07 +0530

    match schedule finish

--- NetClubApi/Modules/MatchModule/MatchBusinessLogic.cs
+++ NetClubApi/Modules/MatchModule/MatchBusinessLogic.cs
@@ -123,9 +123,10 @@ namespace NetClubApi.Modules.MatchModule
                        MatchModel match = await  MatchModelWrapper(
 pair.Key,pair.Value);
                         
-                        await _matchDataAccess.createMatch(match);
+                        string suc=await _matchDataAccess.createMatch(match);
+                        Console.WriteLine(suc);
                         listOfMatch.Add(match);
-                        Console.WriteLine(match.league_id);
+                       // Console.WriteLine(match.league_id);
                     }
 
                 }
